2f96f16b4f856104f60a7e6089b78960e276a4c5
Felicia Udosen <udosenfelicia@gmail.com>
2022-06-07 15:00:52 +0100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='header'>
+        <img 
+        src='images/Troll Face.png'
+        className='header--image'
+        alt='logo'/>
+        <h2 className='header--title'>Meme Generator</h2>
+
+        <h4 className='header--project'>React Course-Project 3</h4>
+
+      
+    </header>
+  )
+}
+
+export default Navbar
